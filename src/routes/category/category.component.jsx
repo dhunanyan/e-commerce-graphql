a672d7bcec38dd661bac0d78d3c2eb2b8e6bfb01
Davit Hunanyan <davit.hunanyan@student.agh.edu.pl>
2022-09-05 16:22:33 +0200

    with-graphql

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect, Fragment } from "react";
+import { useParams } from "react-router-dom";
+// eslint-disable-next-line
+import { gql, useMutation, useQuery } from "@apollo/client";
+
+import ProductCard from "../../components/product-card/product-card.component";
+
+// eslint-disable-next-line
+import { CategoriesContext } from "../../contexts/categories.context";
+
+import { CategoryContainer, Title } from "./category.styles";
+import Spinner from "../../components/spinner/spinner.component";
+
+const GET_CATEGORY = gql`
+  query ($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
+// const SET_CATEGORY = gql`
+//   mutation ($category: Category!) {
+//     addCategory(category: $category) {
+//       id
+//       title
+//       items {
+//         id
+//         name
+//         price
+//         imageUrl
+//       }
+//     }
+//   }
+// `;
+
+const Category = () => {
+  const { category } = useParams();
+  // eslint-disable-next-line
+  const { loading, error, data } = useQuery(GET_CATEGORY, {
+    variables: {
+      title: category,
+    },
+  });
+  // const [addCategory, { loading, error, data }] = useMutation(SET_CATEGORY);
+  // addCategory({
+  //   variables: {
+  //     category: categoryObject,
+  //   },
+  // });
+
+  useEffect(() => {
+    if (data) {
+      const {
+        getCollectionsByTitle: { items },
+      } = data;
+
+      setProducts(items);
+    }
+  }, [category, data]);
+  const [products, setProducts] = useState([]);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Title>{category.toUpperCase()}</Title>
+          <CategoryContainer>
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+          </CategoryContainer>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default Category;
